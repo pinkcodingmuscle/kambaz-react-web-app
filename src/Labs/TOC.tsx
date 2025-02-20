8429ca257be12663cead739ac80f48f7fda0f287
Esther Mukuye <talemwaesther30@gmail.com>
2025-02-19 22:30:03 -0500

    a3 JavaScript

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -14,7 +14,7 @@ export default function TOC() {
       <Nav.Item><Nav.Link as={Link} to ="/Labs/Lab3" id="wd-a3"
         active={pathname.includes("Lab 3")}>Lab 3</Nav.Link></Nav.Item>
       <Nav.Item><Nav.Link as={Link} to ="/Kambaz" id="wd-a3">Kambaz</Nav.Link></Nav.Item>
-      <Nav.Item><Nav.Link href="https://github.com/pinkcodingmuscle" target="_blank">My GitHub</Nav.Link></Nav.Item>
+      <Nav.Item><Nav.Link href="https://github.com/pinkcodingmuscle/kambaz-react-web-app.git" target="_blank">My GitHub</Nav.Link></Nav.Item>
     </Nav>
   );
 }
